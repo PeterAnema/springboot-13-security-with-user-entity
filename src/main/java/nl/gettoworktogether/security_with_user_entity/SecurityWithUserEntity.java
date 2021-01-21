3321708ed67ef0b2908cfae6071c4a33db4135b5
Peter Anema <email@peteranema.nl>
2021-01-21 23:27:28 +0100

    Initial commit

---
+++ src/main/java/nl/gettoworktogether/security_with_user_entity/SecurityWithUserEntity.java
@@ -0,0 +1,13 @@
+package nl.gettoworktogether.security_with_user_entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWithUserEntity {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWithUserEntity.class, args);
+	}
+
+}
---
+++ src/main/java/nl/gettoworktogether/security_with_user_entity/config/SpringSecurityConfig.java
@@ -0,0 +1,53 @@
+package nl.gettoworktogether.security_with_user_entity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Secure the endpoints with HTTP Basic authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+            //HTTP Basic authentication
+            .httpBasic()
+                .and()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/customers/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/admin/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/authenticated/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+            .csrf().disable()
+            .formLogin().disable();
+    }
+
+}
